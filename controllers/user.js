3357e5059287f463e5a70d1781670b130d6e847d
Erik Rood <erikrood@eriks-air.attlocal.net>
2018-03-04 22:00:12 -0800

    home page work

--- controllers/user.js
+++ controllers/user.js
@@ -89,7 +89,9 @@ exports.postSignup = (req, res, next) => {
 
   const user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    subreddits: req.body.subreddits,
+    frequency: req.body.frequency
   });
 
   User.findOne({ email: req.body.email }, (err, existingUser) => {
--- models/User.js
+++ models/User.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
+  subreddits: String,
+  frequency: String,
   passwordResetToken: String,
   passwordResetExpires: Date,
-  user_type: String,
-  customer_id: String,
 
   profile: {
     name: String,
