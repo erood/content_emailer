e3856769bae49b725afbf32cfb3ce99e7b20fc40
Erik Rood <erikrood@eriks-air.attlocal.net>
2018-03-08 20:46:33 -0800

    working data flow to save subreddits, frequency

--- controllers/user.js
+++ controllers/user.js
@@ -127,33 +127,30 @@ exports.getAccount = (req, res) => {
  * Update profile information.
  */
 exports.postUpdateProfile = (req, res, next) => {
-  req.assert('email', 'Please enter a valid email address.').isEmail();
-  req.sanitize('email').normalizeEmail({ remove_dots: false });
 
   const errors = req.validationErrors();
 
   if (errors) {
     req.flash('errors', errors);
-    return res.redirect('/account');
+    return res.redirect('/');
   }
 
   User.findById(req.user.id, (err, user) => {
     if (err) { return next(err); }
-    user.email = req.body.email || '';
-    user.profile.name = req.body.name || '';
-    user.profile.gender = req.body.gender || '';
-    user.profile.location = req.body.location || '';
-    user.profile.website = req.body.website || '';
+    //user.email = req.body.email || '';
+    user.subreddits = req.body.subreddits || '';
+    user.frequency = req.body.frequency || '';
+ 
     user.save((err) => {
       if (err) {
         if (err.code === 11000) {
           req.flash('errors', { msg: 'The email address you have entered is already associated with an account.' });
-          return res.redirect('/account');
+          return res.redirect('/');
         }
         return next(err);
       }
-      req.flash('success', { msg: 'Profile information has been updated.' });
-      res.redirect('/account');
+      req.flash('success', { msg: 'Preferences have been updated.' });
+      res.redirect('/');
     });
   });
 };
